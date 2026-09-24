923cdd6250e84bf2adc1889df8bba4b9ea167667
Federico Ahumada <ahufede@gmail.com>
2026-09-23 21:56:54 -0300

    Upload v0.3

---
+++ assets/chevron-right-BRIF3xmY.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-tD8gJoKN.js";const h=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]),o=e("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C,o as a};
